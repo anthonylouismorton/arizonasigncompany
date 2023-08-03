9c2389d25c2b432676d393f301a2737d8439c024
anthonymorton <anthonymorton760@gmail.com>
2023-08-02 21:20:53 -0700

    misssed one escape character

--- app/Signs/CustomSigns.js
+++ app/Signs/CustomSigns.js
@@ -13,7 +13,7 @@ const CustomSigns = () => {
       <div className="w-1/2 pl-4 pr-4 mt-2">
         <h2 className="text-2xl font-bold text-gray-100 mt-2">Custom Signs</h2>
         <p className="text-gray-100 mt-6">
-        At Arizona Sign Company, we offer end-to-end solutions for businesses of all sizes. Our expertise spans from design to installation, and we take pride in creating iconic signs that leave a lasting impact. Whether it's a small business or a multinational corporation, we treat each client as our top priority, providing exceptional service to all. With a diverse range of satisfied clients, we are dedicated to serving every business with unparalleled dedication and craftsmanship.
+        At Arizona Sign Company, we offer end-to-end solutions for businesses of all sizes. Our expertise spans from design to installation, and we take pride in creating iconic signs that leave a lasting impact. Whether it&apos;s a small business or a multinational corporation, we treat each client as our top priority, providing exceptional service to all. With a diverse range of satisfied clients, we are dedicated to serving every business with unparalleled dedication and craftsmanship.
         </p>
       </div>
     </div>
