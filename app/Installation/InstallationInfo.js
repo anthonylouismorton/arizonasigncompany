85fe31599e74557ec6503b2a5984f3e3d3388e2f
anthonymorton <anthonymorton760@gmail.com>
2023-08-02 21:00:56 -0700

    still working

--- app/Installation/InstallationInfo.js
+++ app/Installation/InstallationInfo.js
@@ -1,12 +1,17 @@
-const InstallationInformation = () => {
+const InstallationInfo = () => {
   return (
-    <div className="flex h-[80vh] bg-gray-800">
-      <div className="w-1/2 pl-4 pr-4 mt-2">
-        <h2 className="text-2xl font-bold text-gray-100 mt-2">Custom Signs</h2>
-        <p className="text-gray-100  mt-6">
+    <div className="grid grid-cols-2">
+      <img
+        src="/installation.jpeg"
+        alt="Installation"
+        className="object-cover h-[100vh]"
+      />
+      <div className="bg-gray-100 p-4">
+        <h2 className="text-2xl font-bold text-gray-800 mt-2">Sign Installation</h2>
+        <p className="text-gray-800 mt-6">
           At Arizona Sign Company, we offer end-to-end solutions for businesses of all sizes. Our expertise spans from design to installation, and we take pride in creating iconic signs that leave a lasting impact. Whether it's a small business or a multinational corporation, we treat each client as our top priority, providing exceptional service to all. With a diverse range of satisfied clients, we are dedicated to serving every business with unparalleled dedication and craftsmanship.
         </p>
-        <p className="text-gray-100 mt-6">
+        <p className="text-gray-800 mt-6">
           In addition to serving our local clientele, Arizona Sign Company has been a trusted partner for sign installation services to companies across the nation. Our reputation for excellence and attention to detail has led businesses from various states to rely on our expertise for their signage needs. From coordinating logistics to ensuring precise installations, our experienced team works diligently to deliver outstanding results and bring brands to life nationwide. As we continue to expand our network of satisfied clients, we remain committed to delivering top-quality service and craftsmanship to businesses throughout the country.
         </p>
       </div>
@@ -14,4 +19,4 @@ const InstallationInformation = () => {
   );
 };
 
-export default InstallationInformation;
+export default InstallationInfo;
--- app/Installation/page.js
+++ app/Installation/page.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import InstallationImage from './InstallationImage';
-import InstallationInformation from './InstallationInformation';
+import InstallationInfo from './InstallationInfo';
+import InstallationForm from './InstallationForm';
 
 export default function Installation() {
   return (
-    <div className="flex bg-gray-100">
-      <div className="w-1/3 pl-4 pr-4 mt-2">
-        <InstallationImage />
-      </div>
-      <div className="w-2/3 pl-4 pr-4 mt-2">
-        <InstallationInformation />
-      </div>
+    <div className="bg-gray-100">
+      <InstallationInfo/>
+      <InstallationForm/>
     </div>
   );
 };
