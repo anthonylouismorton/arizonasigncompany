9bfe3a5433c5d055f9bdcff61db0538fbcde4840
anthonymorton <anthonymorton760@gmail.com>
2023-08-05 18:07:28 -0700

    forgot escape character

--- app/CraneService/CraneInfo.js
+++ app/CraneService/CraneInfo.js
@@ -13,7 +13,7 @@ const CraneInfo = () => {
           <h2 className="text-2xl font-bold text-gray-800 mt-2">Need a lift?</h2>
         </div>
         <p className="text-gray-800 p-6">
-          At Arizona Sign Company, we specialize in providing reliable crane services to support a wide range of industries. Whether it's construction, HVAC, landscaping, or machinery moving, our skilled and certified crane operators are equipped to handle diverse projects with utmost precision and safety. Our commitment to safety is paramount in every crane operation we undertake. We adhere to stringent safety protocols and ensure that all safety measures are in place to protect both our workers and the surrounding environment. With Arizona Sign Company's crane services, you can have peace of mind knowing that your project will be handled with expert care and safety as our top priority.
+          At Arizona Sign Company, we specialize in providing reliable crane services to support a wide range of industries. Whether it&apos;s construction, HVAC, landscaping, or machinery moving, our skilled and certified crane operators are equipped to handle diverse projects with utmost precision and safety. Our commitment to safety is paramount in every crane operation we undertake. We adhere to stringent safety protocols and ensure that all safety measures are in place to protect both our workers and the surrounding environment. With Arizona Sign Company&apos;s crane services, you can have peace of mind knowing that your project will be handled with expert care and safety as our top priority.
         </p>
       </div>
     </div>
