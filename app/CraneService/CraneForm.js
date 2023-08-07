623f0ed74dfceb073f403d124b18f9e8995ac7a2
anthonymorton <anthonymorton760@gmail.com>
2023-08-06 20:17:55 -0700

    update crane service form

--- app/CraneService/CraneForm.js
+++ app/CraneService/CraneForm.js
@@ -1,15 +1,43 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 
 export default function CraneForm() {
+  const [liftInfo, setLiftInfo] = useState({
+    name: '',
+    dateAndTime: '',
+    email: '',
+    phoneNumber: '',
+    description: ''
+  });
+  const [emailSent, setEmailSent] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLiftInfo((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch('/api/crane', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(liftInfo),
+    });
+    setEmailSent(true);
+  };
+
   return (
+    !emailSent ? (
     <div className="bg-gray-800 flex flex-col items-center p-8">
       <h1 className="text-3xl font-semibold text-gray-100 text-center mb-4">
         Interested in scheduling crane service?
       </h1>
       <h6 className="text-lg text-gray-100 text-center mb-6 w-2/3">
-        Please provide your contact information, along with the date, time, and a brief description of the work being performed. Our dedicated crane service scheduler will promptly get in touch with you to assist with your needs.
+        Fill out the form below and one of our dedicated crane service scheduler will promptly get in touch with you to assist with your needs.
       </h6>
-      <form className="w-full max-w-md">
+      <form className="w-full max-w-md" onSubmit={handleSubmit}>
         <div className="mb-4">
           <label htmlFor="name" className="block text-sm font-medium text-gray-100">
             Name
@@ -20,18 +48,22 @@ export default function CraneForm() {
             name="name"
             className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2"
             required
+            value={liftInfo.name}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="name" className="block text-sm font-medium text-gray-100">
+          <label htmlFor="dateAndTime" className="block text-sm font-medium text-gray-100">
             Requested Date/Time of Crane Service
           </label>
           <input
             type="text"
-            id="name"
-            name="name"
+            id="dateAndTime"
+            name="dateAndTime"
             className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2"
             required
+            value={liftInfo.dateAndTime}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-4">
@@ -44,18 +76,22 @@ export default function CraneForm() {
             name="email"
             className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2"
             required
+            value={liftInfo.email}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="number" className="block text-sm font-medium text-gray-100">
+          <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-100">
             Phone Number
           </label>
           <input
             type="text"
-            id="number"
-            name="number"
+            id="phoneNumber"
+            name="phoneNumber"
             className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2"
             required
+            value={liftInfo.phoneNumber}
+            onChange={handleChange}
           />
         </div>
         <div className="mb-4">
@@ -66,19 +102,27 @@ export default function CraneForm() {
             type="text"
             id="description"
             name="description"
-            placeholder="Please provide a brief description of your project (max 300 characters)."
+            placeholder="Please provide a basic description of your project including the time for completion (max 500 characters)."
             rows={3}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-md focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-2"
             required
+            value={liftInfo.description}
+            onChange={handleChange}
           />
         </div>
         <button
           type="submit"
-          className="px-4 py-2 bg-gray-100 text-gray-800 border border-transparent rounded-md font-semibold text-white hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-4"
+          className="px-4 py-2 bg-gray-100 text-gray-700 border-transparent rounded-md font-semibold text-white hover:bg-indigo-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-4"
         >
           Submit
         </button>
       </form>
     </div>
+    ) : (
+    <div className="text-center px-4 py-8 h-72">
+      <h1 className="text-3xl font-semibold pb-6 pt-12 text-gray-800">We received your request!</h1>
+      <p className="text-white">One of our representatives will contact you shortly.</p>
+    </div>
+    )
   );
 };
--- app/CraneService/CraneInfo.js
+++ app/CraneService/CraneInfo.js
@@ -8,11 +8,9 @@ const CraneInfo = () => {
         alt="Installation"
         className="object-cover object-top w-full h-[91vh]"
       />
-      <div className="bg-gray-100 p-4">
-        <div className="flex justify-center">
-          <h2 className="text-2xl font-bold text-gray-800 mt-2">Need a lift?</h2>
-        </div>
-        <p className="text-gray-800 p-6">
+      <div className="bg-gray-100 py-8 px-6">
+        <h2 className="text-2xl font-bold text-gray-800 text-center">Need a lift?</h2>
+        <p className="text-gray-800 pt-8">
           At Arizona Sign Company, we specialize in providing reliable crane services to support a wide range of industries. Whether it&apos;s construction, HVAC, landscaping, or machinery moving, our skilled and certified crane operators are equipped to handle diverse projects with utmost precision and safety. Our commitment to safety is paramount in every crane operation we undertake. We adhere to stringent safety protocols and ensure that all safety measures are in place to protect both our workers and the surrounding environment. With Arizona Sign Company&apos;s crane services, you can have peace of mind knowing that your project will be handled with expert care and safety as our top priority.
         </p>
       </div>
