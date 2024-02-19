95a7abb709f278ec9d289a26a91b5c3b55d15bec
anthonymorton <anthonymorton760@gmail.com>
2024-02-19 14:21:36 -0800

    updated keywords for website

--- app/Signs/layout.js
+++ app/Signs/layout.js
@@ -3,7 +3,7 @@ import React from "react";
 export const metadata = {
   title: 'Signs',
   description: 'Signs',
-  keywords: 'signs, sign manufacturing, billboards, led boards, sign repair, quote'
+  keywords: 'signs, sign manufacturing, billboards, led boards, estimate, quote'
 }
 
 export default function Signs({ children }) {
--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Arizona Sign Company',
   description: 'Sign Manufacturing and Crane Service',
-  keywords: 'crane service, Arizona Sign Company, Arizona Sign Co, signs, led boards, outdoor billboards'
+  keywords: 'arizona sign company, arizona sign companies, az sign company, kingman signs, arizona signs'
 }
 
 export default function RootLayout({ children }) {
