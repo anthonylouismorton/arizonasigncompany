3f0d6f69f43b93737809da5f9ecd4159f95de8e4
Anthony Morton <anthonymorton760@gmail.com>
2024-12-10 23:58:36 -0800

    fixed formatting for mobile and smaller screens

--- app/page.js
+++ app/page.js
@@ -17,31 +17,33 @@ export default function Home() {
             priority={true}
             className="max-w-full object-top brightness-50"
           />
-          <div className="absolute inset-x-40 inset-y-40">
+          <div className="absolute inset-x-4 inset-y-4 sm:inset-x-40 sm:inset-y-40 ">
             <div className="text-white">
-              <h1 className="md:text-6xl sm:text-4xl font-bold mb-6">
+              <h1 className="md:text-6xl sm:text-4xl text-xl font-bold sm:mb-6 mb-1">
                 Visibility Starts Here
               </h1>
-              <p className="md:text-4xl sm:text-2xl font-bold mb-20">
+              <p className="md:text-4xl sm:text-2xl text-xl font-bold mb-32 sm:mb-20">
                 We Specialize in Quality Custom Signs.
               </p>
-              <div className="flex space-x-4">
-                <a
-                  href="/Signs#quote" 
-                  className="bg-red-600 text-white py-3 px-5 rounded-md text-lg hover:bg-red-700 transition flex items-center space-x-2"
-                >
-                  <span>Get Started on Your Vision</span>
-                  <i class="fa fa-chevron-right text-xl"></i>
-
-
-                </a>
-                <a
+              <div className="flex flex-col sm:flex-row space-y-4 sm:space-x-4">
+                <div className="inline-flex">
+                  <a
+                    href="/Signs#quote"
+                    className="bg-red-600 text-white py-3 px-5 rounded-md text-md sm:text-lg font-semibold hover:bg-red-700 transition flex items-center space-x-2"
+                  >
+                    <span>Get Started on Your Vision</span>
+                    <i className="fa fa-chevron-right text-xl"></i>
+                  </a>
+                </div>
+                <div className="inline-flex">
+                  <a
                     href="tel:+1-760-207-6587"
-                    className="bg-header-yellow text-black py-3 px-5 rounded-md text-lg font-semibold hover:text-white transition flex items-center space-x-2"
+                    className="bg-header-yellow text-black py-3 px-5 rounded-md text-md sm:text-lg font-semibold hover:text-white transition flex items-center space-x-2"
                   >
                     <i className="fa fa-phone"></i>
                     <span>928-303-1300</span>
                   </a>
+                </div>
               </div>
             </div>
           </div>
